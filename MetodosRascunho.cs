6cec8489a06076d25a425b2bb2f66676e7394625
jessicapcosta <jessicacosta194@gmail.com>
2021-11-24 21:20:00 +0000

    Alteracao estado

---
+++ MetodosRascunho.cs
@@ -0,0 +1,104 @@
+﻿/*
+*	<copyright file="TrabalhoFinal" company="IPCA">
+*		Copyright (c) 2021 All Rights Reserved
+*	</copyright>
+* 	<author>Jéssica Costa 20872</author>
+*   <date>11/24/2021 9:12:22 PM</date>
+*	<description> </description>
+**/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Purpose:
+/// Created by:Jéssica Costa 20872
+/// Created on:11/24/2021 9:12:22 PM
+/// </summary>
+/// 
+
+namespace TrabalhoFinal
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: Jéssica Costa 20872
+    /// Created on: 11/24/2021 9:12:22 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    class MetodosRascunho
+    {
+        #region Atributos
+
+        #endregion
+
+
+        #region Methods
+
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Overrides
+
+        #endregion
+
+        #region OtherMethods
+        /*public bool RegistaEstadoUtente(Estado e, string numero)
+        {
+            foreach (Utente u in doentes)
+            {
+                if (u.NumUtente == numero)
+                {
+                    return u.RegistaEstdoUtente(e);
+                }
+            }
+            return false;
+        }*/
+
+
+        /// <summary>
+        /// Novo estado do doente
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns>True or False</returns>
+        /*public bool RegistaEstdoUtente(Estado e)
+        {
+            return this.AddEstado(e);
+            
+        }*/
+
+        /// <summary>
+        /// Adiciona estado com respetiva situação clínica e data atual
+        /// </summary>
+        /// <param name="novaSit"></param>
+        /// <param name="novaData"></param>
+        /// <returns></returns>
+        /*public bool AddEstado( Situacao novaSit, DateTime novaData)
+        {
+            if (n == null) n = new List<Estado>();
+            Estado novo = new Estado();
+            novo.DataNovoEstado = novaData;
+            novo.Sit = novaSit;
+
+            n.Add(novo);
+            return true;
+        }*/
+
+        #endregion
+
+
+
+        #endregion
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -20,27 +20,42 @@ namespace TrabalhoFinal
 
         {
             
-            Utente a1 = new Utente("123456789","Ana", "09-12-1975", Genero.FEMININO, Distrito.Braga, Situacao.ALTA);
-            Utente a2 = new Utente("119234562", "Sérgio Martins", "07-12-81", Genero.MASCULINO, Distrito.Porto, Situacao.LIGEIRO);
+           
+            //Utente a2 = new Utente("119234562", "Sérgio Martins", "07-12-81", Genero.MASCULINO, Distrito.Porto, Situacao.LIGEIRO);
 
-            a1.AddEstado(Situacao.LIGEIRO, DateTime.Today );
+            //a1.AddEstado(Situacao.LIGEIRO, DateTime.Today );
             Utentes hospital = new Utentes();
 
-            hospital.AddUTente(a1);
+            //hospital.AddUTente(a1);
 
 
 
 
-            
-            Console.WriteLine(a1.Nome);
-            Console.WriteLine(a1.GeneroU);
-            Console.WriteLine(a1.DataNascimento.ToString());
-            
+
+            //Console.WriteLine(a1.Nome);
+            //Console.WriteLine(a1.GeneroU);
+            //Console.WriteLine(a1.DataNascimento.ToString());
+
             //Console.WriteLine(b.ContaDias(123456789));
-          
-            Console.WriteLine(a1.CalculaIdade().ToString());
 
+            //Console.WriteLine(a1.CalculaIdade().ToString());
+            DateTime date = Convert.ToDateTime("02/09/20");
+            Estado estado1 = new Estado(date, Situacao.LIGEIRO);
+            Estado estado2 = new Estado(date, Situacao.ALTA);
+            
+            Utente a1 = new Utente("123456789", "Ana", "09-12-1975", Genero.FEMININO, Distrito.Braga, estado2);
+            Utente a2 = new Utente("123156789", "Pedro", "09-12-1975", Genero.MASCULINO, Distrito.Braga, estado1);
+            a1.AddEstado(estado1);//adiciona um estado a lista de um utente
+            a2.AddEstado(estado2);
+            Utentes u = new Utentes();
 
+            u.AddUTente(a1); 
+            u.AddUTente(a2);
+            u.MostraListaNomeUtente();
+            a1.MostraEstadoUtente();
+            a2.MostraEstadoUtente();
+            Console.WriteLine(u.MostraNomeUtente("123456789"));
+            //u.RegistaEstadoUtente(estado1, "123456789");
 
             Console.ReadKey();
 
