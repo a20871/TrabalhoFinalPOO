a8b5dae531bb8873d803f061197738c867215311
jessicapcosta <jessicacosta194@gmail.com>
2021-11-27 19:32:05 +0000

    alteracoes

---
+++ UtenteInternado.cs
@@ -0,0 +1,65 @@
+﻿/*
+*	<copyright file="TrabalhoFinal" company="IPCA">
+*		Copyright (c) 2021 All Rights Reserved
+*	</copyright>
+* 	<author>Jéssica Costa 20872</author>
+*   <date>11/27/2021 6:46:03 PM</date>
+*	<description> </description>
+**/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Purpose:
+/// Created by:Jéssica Costa 20872
+/// Created on:11/27/2021 6:46:03 PM
+/// </summary>
+/// 
+
+namespace TrabalhoFinal
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: Jéssica Costa 20872
+    /// Created on: 11/27/2021 6:46:03 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    class UtenteInternado
+    {
+        #region Atributos
+
+        #endregion
+
+
+        #region Methods
+
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Overrides
+
+        #endregion
+
+        #region OtherMethods
+
+
+        #endregion
+
+
+
+        #endregion
+    }
+
+
+}
